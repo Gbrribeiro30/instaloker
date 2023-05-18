7f75a4a05048a40e74a3d74e9211a90f0831f0ad
Gbrribeiro30 <133721315+Gbrribeiro30@users.noreply.github.com>
2023-05-17 22:02:13 -0300

    Adicionado mais opções de bonecos

--- instaloker.py
+++ instaloker.py
@@ -1,11 +1,22 @@
 import pyautogui;
 import time;
-#confirmar x=959, y=816
-#jett x=1205, y=924
+confirmar =  (959, 816)
+jett = (1205, 924)
+reyna = (881, 1000)
+raze = (784, 1012)
+#time.sleep(5)
 #print(pyautogui.position())
-time.sleep(5)
 i = 0
+seleção = input("selecione seu boneco\n (1)Jett\n (2)Reyna\n (3)Raze\n")
+if seleção == "1":
+    boneco = jett
+elif seleção == "2":
+    boneco = reyna
+elif seleção == "3":
+    boneco = raze
+
+time.sleep(5) 
 while i < 30:
-    pyautogui.click(x=1205, y=924)
-    pyautogui.click(x=959, y=816)
+    pyautogui.click(boneco) 
+    pyautogui.click(confirmar)
     i = i+1
